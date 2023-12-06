dc161d5a2bf0bb57e32a4766fed4f43afb743b38
dantebrie <gappinheiro@hotmail.com>
2023-12-05 22:59:55 -0300

    tela de cadastro remodelada com validações e botão de voltar para login

--- app/src/main/java/com/example/recomendador_de_filmes/RegisterScreen.java
+++ app/src/main/java/com/example/recomendador_de_filmes/RegisterScreen.java
@@ -3,19 +3,35 @@ package com.example.recomendador_de_filmes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.recomendador_de_filmes.Util.ConfigFirebase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
 public class RegisterScreen extends AppCompatActivity {
-    private TextView emailField, passwordField, confirmPasswordField;
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[\\S+$])" +
+                    ".{6,}" +
+                    "$");
+
+    private TextInputEditText emailField, passwordField, confirmPasswordField;
+    private TextInputLayout emailLayout, passwordLayout, confirmPasswordLayout;
+    Button backButton;
     FirebaseAuth auth;
     Users user;
 
@@ -23,29 +39,83 @@ public class RegisterScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+
         emailField = findViewById(R.id.emailField);
         passwordField = findViewById(R.id.passwordField);
         confirmPasswordField = findViewById(R.id.confirmPasswordField);
+        emailLayout = findViewById(R.id.emailLayout);
+        passwordLayout = findViewById(R.id.passwordLayout);
+        confirmPasswordLayout = findViewById(R.id.confirmPasswordLayout);
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         user = new Users();
     }
 
+    public boolean validateEmail () {
+        String email = emailField.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailLayout.setError("O campo não pode estar vazio.");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailLayout.setError("Informe um e-mail válido.");
+            return false;
+        } else {
+            emailLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword () {
+        String password = passwordField.getText().toString().trim();
+
+        if (password.isEmpty()) {
+            passwordLayout.setError("O campo não pode estar vazio.");
+            return false;
+        } else if(!PASSWORD_PATTERN.matcher(password).matches()) {
+            passwordLayout.setError("Senha muito fraca. Sua senha precisa de ao menos 6 caracteres");
+            return false;
+        }else {
+            passwordLayout.setError(null);
+            return true;
+        }
+    }
+
+    public boolean validateConfirmPassword () {
+        String confirmPassword = confirmPasswordField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (confirmPassword.isEmpty()) {
+            confirmPasswordLayout.setError("O campo não pode estar vazio.");
+            return false;
+        } else if (!confirmPassword.equals(password)) {
+            confirmPasswordLayout.setError("As senhas não correspondem.");
+            return false;
+        }else {
+            confirmPasswordLayout.setError(null);
+            return true;
+        }
+    }
+
     public void validateFields(View view) {
-        String email = emailField.getText().toString();
-        String password = passwordField.getText().toString();
-        String confirmPassword = confirmPasswordField.getText().toString();
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (!validateEmail() | !validatePassword() | !validateConfirmPassword()) {
+            return;
+        }
 
         user.setEmail(email);
-
-        if (password.equals(confirmPassword)) {
-            user.setPassword(password);
-        }
-        else {
-            Toast.makeText(this, "As senhas estão diferentes!", Toast.LENGTH_SHORT).show();
-        }
-
-        if (!email.isEmpty() || !password.isEmpty()) {
-            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
-        }
+        user.setPassword(password);
 
         registerUser();
     }
@@ -60,7 +130,7 @@ public class RegisterScreen extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(RegisterScreen.this, "Registro bem sucedido!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterScreen.this, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(RegisterScreen.this, "Erro ao cadastrar: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
