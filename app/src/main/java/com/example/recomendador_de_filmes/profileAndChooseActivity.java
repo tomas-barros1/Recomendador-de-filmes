de418757c3c056f14c8b19024a9c0fc01d6ff116
Gabriel Pinheiro <131053030+dantebrie@users.noreply.github.com>
2023-12-10 16:04:30 -0300

    exibir username e email

--- app/src/main/java/com/example/recomendador_de_filmes/profileAndChooseActivity.java
+++ app/src/main/java/com/example/recomendador_de_filmes/profileAndChooseActivity.java
@@ -1,42 +1,39 @@
 package com.example.recomendador_de_filmes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class profileAndChooseActivity extends AppCompatActivity {
-    private TextView name, email;
-    private ImageButton selectMovie, selectBook, selectSerie, selectAnimation;
+    private static final String SHARED_PREFS = "SharedPrefs";
+    TextView usernameProfile, emailProfile;
+    ImageButton goToMovies;
+    Button logOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_and_choose);
-        name = findViewById(R.id.profileName);
-        email = findViewById(R.id.profileEmail);
-        selectMovie = findViewById(R.id.selectMovie);
-        selectBook = findViewById(R.id.selectBook);
-        selectSerie= findViewById(R.id.selectShow);
-        selectAnimation = findViewById(R.id.selectCartoon);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        usernameProfile = findViewById(R.id.profileName);
+        emailProfile = findViewById(R.id.profileEmail);
+        goToMovies = findViewById(R.id.selectMovie);
 
-        if (user != null) {
-            String userEmail = user.getEmail();
+        logOut = findViewById(R.id.logOut);
 
-            email.setText(userEmail);
+        showUserData();
 
-        } else {
-            Toast.makeText(getApplicationContext(), "Usuário não autenticado", Toast.LENGTH_SHORT).show();
-        }
-        selectMovie.setOnClickListener(new View.OnClickListener() {
+        goToMovies.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ParametersActivity1.class);
@@ -45,29 +42,36 @@ public class profileAndChooseActivity extends AppCompatActivity {
             }
         });
 
-        selectBook.setOnClickListener(new View.OnClickListener() {
+        logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Em desenvolvimento!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                clearLoginData();
 
-        selectSerie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Em desenvolvimento!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                FirebaseAuth.getInstance().signOut();
 
-        selectAnimation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Em desenvolvimento!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
     }
 
+    public void showUserData() {
+        Intent intent = getIntent();
 
+        String nameUser = intent.getStringExtra("username");
+        String emailUser = intent.getStringExtra("email");
+
+        usernameProfile.setText(nameUser);
+        emailProfile.setText(emailUser);
+    }
+
+    private void clearLoginData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
 
 }
