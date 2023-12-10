e46e099331f6140a06cba4310d640ed96aa755ea
Gabriel Pinheiro <131053030+dantebrie@users.noreply.github.com>
2023-12-10 16:09:39 -0300

    cadastro indo para o firebase

--- app/src/main/java/com/example/recomendador_de_filmes/RegisterScreen.java
+++ app/src/main/java/com/example/recomendador_de_filmes/RegisterScreen.java
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.recomendador_de_filmes.Util.ConfigFirebase;
@@ -18,6 +18,8 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Pattern;
 
@@ -29,24 +31,30 @@ public class RegisterScreen extends AppCompatActivity {
                     ".{6,}" +
                     "$");
 
-    private TextInputEditText emailField, passwordField, confirmPasswordField;
+    private TextInputEditText usernameField, emailField, passwordField, confirmPasswordField;
     private TextInputLayout emailLayout, passwordLayout, confirmPasswordLayout;
-    Button backButton;
+    Button button, backButton;
     FirebaseAuth auth;
     Users user;
+    FirebaseDatabase fireDataBase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
 
+        usernameField = findViewById(R.id.usernameField);
         emailField = findViewById(R.id.emailField);
         passwordField = findViewById(R.id.passwordField);
         confirmPasswordField = findViewById(R.id.confirmPasswordField);
         emailLayout = findViewById(R.id.emailLayout);
         passwordLayout = findViewById(R.id.passwordLayout);
         confirmPasswordLayout = findViewById(R.id.confirmPasswordLayout);
+        button = findViewById(R.id.button);
         backButton = findViewById(R.id.backButton);
+        fireDataBase = FirebaseDatabase.getInstance();
+        databaseReference = fireDataBase.getReference("usuarios");
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,8 +128,23 @@ public class RegisterScreen extends AppCompatActivity {
         registerUser();
     }
 
-    public void registerUser() {
+    public void saveUserOnDatabase() {
+        String username = usernameField.getText().toString();
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
 
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterScreen.this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
+        } else {
+            Users users = new Users(username, email, password);
+            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            databaseReference.child(uid).setValue(users);
+
+            Toast.makeText(RegisterScreen.this, "Conta adicionada com sucesso!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void registerUser() {
         auth = ConfigFirebase.FirebaseAuthentication();
 
         auth.createUserWithEmailAndPassword(
@@ -131,6 +154,7 @@ public class RegisterScreen extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     Toast.makeText(RegisterScreen.this, "Conta criada com sucesso!", Toast.LENGTH_SHORT).show();
+                    saveUserOnDatabase();
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                 } else {
@@ -139,5 +163,4 @@ public class RegisterScreen extends AppCompatActivity {
             }
         });
     }
-
 }
